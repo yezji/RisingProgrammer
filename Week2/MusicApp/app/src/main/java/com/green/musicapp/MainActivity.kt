bed18fbd0c92c27d2c57bafd61bcfaa2106b2936
yezji <yeji.dev@gmail.com>
2021-01-07 17:42:35 +0900

    add android files

--- Week2/MusicApp/app/src/main/java/com/green/musicapp/MainActivity.kt
+++ Week2/MusicApp/app/src/main/java/com/green/musicapp/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.green.musicapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "MainActivity.onCreate()", Toast.LENGTH_SHORT).show()
+
+        val intent = Intent(this, DjCollectionActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "MainActivity.onRestart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "MainActivity.onStart()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "MainActivity.onResume()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "MainActivity.onPause()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "MainActivity.onStop()", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "MainActivity.onDestroy()", Toast.LENGTH_SHORT).show()
     }
 }
